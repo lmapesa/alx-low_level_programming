d6327d45664ecc7d54dc7de117fb97f22062d13b
lmapesa <levinmapesa@gmail.com>
2023-03-17 11:46:05 -0700

    c intro

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void) {
+  char c;
+
+  for (c = 'a'; c <= 'z'; c++) {
+    putchar(c);
+  }
+  for (c = 'A'; c <= 'Z'; c++) {
+    putchar(c);
+  }
+  putchar('\n');
+  return 0;
+}
