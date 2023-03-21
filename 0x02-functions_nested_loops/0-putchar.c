ecedde87bcb5df1d3e2c9702f17935f749495aac
lmapesa <levinmapesa@gmail.com>
2023-03-21 11:57:03 -0700

    q0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints "_putchar" followed by a new line.
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void) {
+	printf("_putchar\n");
+	return 0;
+}
