77726f40de068edd0732a5593ce06d18a03ea1c9
lmapesa <levinmapesa@gmail.com>
2023-04-13 14:59:48 -0700

    gcc

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checTo check 4 uppercase character
+ * @c: character to be checked.
+ * Return: 1 if c is uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
